efb1f36ab3d843fbdc950b737e52548e93fdc0d8
Conrado Halmenschlager <90523295+ConradoHVieira@users.noreply.github.com>
2022-08-03 17:37:12 -0300

    03.08

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import Botao from "../components/Botao"
+import Formulario from "../components/Formulario"
 import Layout from "../components/Layout"
 import Tabela from "../components/Tabela"
 import Cliente from "../core/Cliente"
+
 export default function Home() {
   const clientes = [
     new Cliente('Ana', 14, '1'),
@@ -8,7 +11,13 @@ export default function Home() {
     new Cliente('Pedro', 11, '3'),
     new Cliente('Luiza', 52, '4'),
   ]
-  
+  function clienteSelecionado(cliente: Cliente) {
+
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+
+  }
   return (
     <div className={`
     flex justify-center items-center h-screen
@@ -16,9 +25,16 @@ export default function Home() {
     text-white
     `}>
       <Layout titulo="Cadastro de Usuário">
-        <Tabela clientes={clientes}></Tabela>
+        <div className="flex justify-end">
+          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
+         <Tabela clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
+      <Formulario cliente={clientes[0]}></Formulario>
       </Layout>
-      
+
     </div>
   )
 }
